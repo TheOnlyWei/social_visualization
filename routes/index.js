4779fbe0d3e58a96b052d5a4854c403acde13ec3
Nathan <dobobobado@gmail.com>
2017-02-15 16:36:07 -0500

    added twit update sample, integrated mongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var twit = require('twit'); //module for interacting with Twitter API
-var config = require('../config/config') //config.js contains keys for twitter API access
+var config = require('../config/config'); //config.js contains keys for twitter API access
+var mongodb = require('mongodb');
 
-var T = twit(config); //twitter config
+var T = twit(config.twitter); //twitter config
 
 //max get requests is 1 every 5 seconds
 function checkLimit(key) {
@@ -38,23 +39,88 @@ function getTweetSample() {
     //https://dev.twitter.com/rest/reference/get/search/tweets
     var getParams = {
       q: 'movie since:2011-11-11',
-      count: 10
+      count: 1
     }
     function gotData(err, data, response) {
+      var MongoClient = mongodb.MongoClient;
+      var mongoDB_URL = config.mongodb.url;
       var tweets = data.statuses;
+
+      MongoClient.connect(mongoDB_URL,function(err, db){
+        if(err){
+          console.log("Unable to connect, error: ", err);
+          res.send(err);
+
+        } else {
+          var collection = db.collection('movies');
+          collection.insert(tweets, function(err, result){
+            if(err){
+              console.log(err);
+            } else {
+              console.log('succesful insertion');
+            }
+            db.close();
+          });
+        }
+
+      });
+      /*
       tweets.forEach(function(t){
         tweetData.push(t.text);
         console.log('TWEET: ' + t.text);
-
       });
-      //console.log(data);
+      console.log(data);
+      */
     };
     //search request
     //params: what to search for
     //gotData: callback function called once data is retrieved
-    T.get('search/tweets', getParams, gotData), 5000;
+    T.get('search/tweets', getParams, gotData);
 }
 
+//updates one tweet over and over again.
+function updateTweetSample() {
+    //q is the query term parameter
+    //get 10 latest tweets since 2011-11-11
+    //go to the below link for a list of search params
+    //https://dev.twitter.com/rest/reference/get/search/tweets
+    var getParams = {
+      q: 'movie since:2011-11-11',
+      count: 1
+    }
+    function gotData(err, data, response) {
+      var MongoClient = mongodb.MongoClient;
+      var mongoDB_URL = config.mongodb.url;
+      var tweets = data.statuses;
+
+      MongoClient.connect(mongoDB_URL,function(err, db){
+        if(err){
+          console.log("Unable to connect, error: ", err);
+          res.send(err);
+
+        } else {
+          var collection = db.collection('movies');
+          collection.update({}, data, {upsert:true});
+          db.close();
+        }
+
+      });
+      /*
+      tweets.forEach(function(t){
+        tweetData.push(t.text);
+        console.log('TWEET: ' + t.text);
+      });
+      console.log(data);
+      */
+    };
+    //search request
+    //params: what to search for
+    //gotData: callback function called once data is retrieved
+    T.get('search/tweets', getParams, gotData);
+}
+
+
+getTweetSample();
 //setInterval(function(){checkLimit('search');}, 1000*10);
 //checkLimit('search'); //gets how many API calls you have left for the current 15 minutes
 
@@ -62,13 +128,14 @@ function getTweetSample() {
 //   STREAM
 ////////////////
 //filter public stream by English tweets containing #apple
-var stream = T.stream('statuses/filter', {track: '#apple', language: 'en'});
+//var stream = T.stream('statuses/filter', {track: '#apple', language: 'en'});
 //whenever there is a tweet with the above stream parameters, the callback
 //function is executed
+/*
 stream.on('tweet', function(tweet) {
   console.log(tweet);
 });
-
+*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
